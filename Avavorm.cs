895eaaa340f369438c0bae0ef55948a34a470f0a
opilane <opilane@HP-CZC2349HTC>
2026-09-02 17:46:09 +0300

    Add project files.

---
+++ Avavorm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Naidis_IKTpv25_Windows_Forms
+{
+    
+    public partial class Avavorm : Form
+    {
+        TreeView tree;
+        Button nupp;
+        Label silt;
+        PictureBox pilt;
+        public Avavorm()
+        {
+            Height = 600;
+            Width = 1000;
+            Text = "Naidis IKTpv25 Windows Forms";
+            tree = new TreeView();
+            tree.Dock = DockStyle.Left;
+            tree.AfterSelect += Tree_AfterSelect;
+            TreeNode tn = new TreeNode("Elemendid");
+            tn.Nodes.Add(new TreeNode("Nupp"));
+            tn.Nodes.Add(new TreeNode("Silt"));
+            tn.Nodes.Add(new TreeNode("Pilt"));
+            tree.Nodes.Add(tn);
+
+            //nupp,silt ja pilt
+            nupp = new Button();
+            nupp.Text = "Vajuta Siia"; 
+            nupp.Location = new Point(300, 100);
+            nupp.Height = 50;
+            nupp.Width = 100;
+            nupp.Click += (sender, e) => { MessageBox.Show("Nupp vajutati!"); };
+
+            silt = new Label();
+            silt.Text = "See on silt";
+            silt.Location = new Point(300,200);
+            silt.Size = new Size(200, 30);
+            silt.Font = new Font("Arial", 16, FontStyle.Bold);
+            silt.AutoSize = true;
+            silt.MouseLeave += Silt_MouseLeave;
+            silt.MouseHover += Silt_MouseHover;
+
+            pilt = new PictureBox();
+            pilt.Image = Image.FromFile(@"..\..\Pildid\images.jpg");
+            pilt.Location = new Point(300, 300);
+            pilt.Size = new Size(200, 200);
+            pilt.SizeMode = PictureBoxSizeMode.StretchImage;
+            pilt.MouseDoubleClick += Pilt_MouseDoubleClick;
+
+
+            Controls.Add(tree);
+        }
+
+        private void Pilt_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Size väike = new Size(200, 200);
+            Size suur = new Size(800, 50);
+            if (pilt.Size == suur)
+                pilt.Size = väike;
+
+            else
+                pilt.Size = suur;
+            
+        }
+
+        private void Silt_MouseHover(object sender, EventArgs e)
+        {
+            silt.BackColor = Color.LightGray;
+            silt.BorderStyle = BorderStyle.Fixed3D;
+        }
+
+        private void Silt_MouseLeave(object sender, EventArgs e)
+        {
+            silt.BackColor = Color.Black;
+            
+        }
+
+        private void Tree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Text == "Nupp")
+            {
+                Controls.Add(nupp);
+                tree.SelectedNode = null;
+            }
+            else if (e.Node.Text == "Silt")
+            {
+                Controls.Add(silt);
+                tree.SelectedNode = null;
+            }
+            else if (e.Node.Text == "Pilt")
+            {
+                Controls.Add(pilt);
+                tree.SelectedNode = null;
+            }
+        }
+    }
+}
